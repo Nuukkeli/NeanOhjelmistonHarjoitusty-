fe5549f13dc39344b80268452d3a69004d27f87f
Nuukkeli <nea.rantanen@helsinki.fi>
2016-09-15 17:16:19 +0300

    Ensimmäinen luokka

--- NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/peli.java
+++ NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/peli.java
@@ -3,11 +3,19 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package muistipeli.neanmuistipeli;
 
 /**
  *
- * @author root
+ * @author euro
  */
-public class peli {
-
+public class Peli {
+    
+    public Peli(){
+        
+    }
+    
+    public void aloitaPeli(){
+        System.out.println("Aloitetaan!");
+    }
 }
