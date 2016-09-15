4436407390abdf3608319d153c9a0fe46eeb0803
Nuukkeli <nea.rantanen@helsinki.fi>
2016-09-15 17:15:32 +0300

    Main luotu

---
+++ NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/PeliMain.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package muistipeli.neanmuistipeli;
+/**
+ *
+ * @author euro
+ */
+public class PeliMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Peli peli = new Peli();
+        peli.aloitaPeli();
+    }
+    
+}
