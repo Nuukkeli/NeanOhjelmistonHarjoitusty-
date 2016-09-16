df16ddd41a2b503ea4d0ef3a6edd7453e3fcab8b
Nuukkeli <nea.rantanen@helsinki.fi>
2016-09-16 14:15:14 +0300

    Huomautuksia lisätty

--- NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/Pelialusta.java
+++ NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/Pelialusta.java
@@ -1,17 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package muistipeli.neanmuistipeli;
 
-/**
- *
- * @author euro
- */
+//TÄMÄ LUOKKA MUUTTUU PALJON SIIRRYTTÄESSÄ POIS PELKÄSTÄ TEKSTISTÄ. Hieman ajatusta tähän vain hain.
+
 public class Pelialusta {
     private Korttipakka pakka;
     private int kortteja;
+    
     
     public Pelialusta(Korttipakka korttipakka){
         this.pakka = korttipakka;
