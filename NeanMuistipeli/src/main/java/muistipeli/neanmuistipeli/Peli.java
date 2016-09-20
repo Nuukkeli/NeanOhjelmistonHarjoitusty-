aba2366b8b68606893a0ebc45a18810bef2ca4d7
Nuukkeli <nea.rantanen@helsinki.fi>
2016-09-20 14:02:12 +0300

    Pientä muokkausta

--- NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/Peli.java
+++ NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/Peli.java
@@ -1,16 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package muistipeli.neanmuistipeli;
 
 import java.util.Scanner;
 
-/**
- *
- * @author euro
- */
 public class Peli {
     
     private Pelialusta alusta;
