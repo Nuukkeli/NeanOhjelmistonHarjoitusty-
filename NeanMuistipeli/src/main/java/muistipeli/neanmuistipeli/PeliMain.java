87b4eb9ea278f0423aa7eb6bf5fa5ae920bff320
Nuukkeli <nea.rantanen@helsinki.fi>
2016-09-23 16:25:31 +0300

    Peliä testailtu

--- NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/PeliMain.java
+++ NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/PeliMain.java
@@ -1,18 +1,19 @@
 package muistipeli.neanmuistipeli;
 
+import muistipeli.neanmuistipeli.peli.Peli;
+
 public class PeliMain {
 
     /**
      * @param args the command line arguments
      */
-    
     //Muistipeli toimii, mutta käyttöliittymää en ole vielä luonut. 
     //Itseäni vain helpottaa toteuttaa projekti ensin näin, jotta toiminta hahmottuu.
     public static void main(String[] args) {
 
-        Peli peli = new Peli(5); //Korttien määrän voisi myös kysyä pelin alussa. 
+        Peli peli = new Peli(2); //Korttien määrän voisi myös kysyä pelin alussa. 
         peli.pelaa();
-        
+
     }
 
 }
