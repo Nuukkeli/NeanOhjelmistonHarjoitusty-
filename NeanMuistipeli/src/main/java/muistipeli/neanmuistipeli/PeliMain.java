6504ef64e888e3496cafe26395d1f8dad76537b7
Nuukkeli <nea.rantanen@helsinki.fi>
2016-10-11 12:12:58 +0300

    Pelin aloitus muuttunut

--- NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/PeliMain.java
+++ NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/PeliMain.java
@@ -14,7 +14,7 @@ public class PeliMain {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Peli peli = new Peli(10, false);
+        Peli peli = new Peli();
         peli.aloita();
     }
 
--- NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/peli/Peli.java
+++ NeanMuistipeli/src/main/java/muistipeli/neanmuistipeli/peli/Peli.java
@@ -5,36 +5,134 @@
  */
 package muistipeli.neanmuistipeli.peli;
 
-//MUUTTUU JA PALJON! TARKOITUS, ETTÄ SAA VALITA PARIEN MÄÄRÄN (NAPPULAT) JA PELIMUODON.
+
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.*;
+
 
 /**
  * Luokka käynnistää pelin. Myöhemmin tekee varmasti muutakin.
  */
-public class Peli {
+public class Peli extends JFrame implements ActionListener {
 
-    private Pelialusta alusta;
+    private Pelialusta pelialusta;
     int pareja;
     boolean pelaaPariMuistipeli;
-
+    JFrame alusta;
+    private JButton helppo;
+    private JButton keskitaso;
+    private JButton vaikea;
+    private JButton haaste;
+    private JButton pari;
+    private JButton jarjestys;
+    private JButton aloita;
+    
     /**
      * Luokan konstruktori.
      *
-     * @param parienMaara Korttiparien määrä, joka halutaan peliin.
-     * 
-     * @param pelataankoPariMuistipeli Kertoo pelataanko muistipeli, jossa 
-     * etsitään pareja.
      */
-    public Peli(int parienMaara, boolean pelataankoPariMuistipeli) {
-        pareja = parienMaara;
-        pelaaPariMuistipeli = pelataankoPariMuistipeli;
+    public Peli() {
+        pareja = 2;
+        pelaaPariMuistipeli = true;
     }    
 
     /**
      * Metodi aloittaa pelin.
      */
     public void aloita() {
-        alusta = new Pelialusta(pareja, pelaaPariMuistipeli);
-        alusta.pelaa();
+        this.kysymysAlusta();
+    }
+    
+    public void kysymysAlusta(){
+        alusta = new JFrame("Valitse peli ja vaikeustaso");
+        alusta.setPreferredSize(new Dimension(500, 500));
+        alusta.setLayout(new GridLayout(3, 1));
+        
+        JPanel pelimuotopaneeli = new JPanel(new GridLayout(4, 4));
+        
+        ButtonGroup peli = new ButtonGroup();
+        ButtonGroup vaikeus = new ButtonGroup();
+        
+        jarjestys = new JButton("Järjestys muistipeli");
+        jarjestys.addActionListener(this);
+        pari = new JButton("Pari muistipeli");
+        pari.addActionListener(this);
+        
+        pelimuotopaneeli.add(new JLabel("Valitse pelimuoto:"));
+        
+        peli.add(pari);
+        pelimuotopaneeli.add(pari);
+        peli.add(jarjestys);
+        pelimuotopaneeli.add(jarjestys);
+        
+        helppo = new JButton("Helppo");
+        helppo.addActionListener(this);
+        keskitaso = new JButton("Keskitaso");
+        keskitaso.addActionListener(this);
+        vaikea = new JButton("Vaikea");
+        vaikea.addActionListener(this);
+        haaste = new JButton("HAASTE");
+        haaste.addActionListener(this);
+        
+        JPanel vaikeustasopaneeli = new JPanel(new GridLayout(4,4));
+                
+        pelimuotopaneeli.add(new JLabel("Valitse vaikeustaso: "));
+        
+        vaikeus.add(helppo);
+        vaikeustasopaneeli.add(helppo);
+        vaikeus.add(keskitaso);
+        vaikeustasopaneeli.add(keskitaso);
+        vaikeus.add(vaikea);
+        vaikeustasopaneeli.add(vaikea);
+        vaikeus.add(haaste);
+        vaikeustasopaneeli.add(haaste);
+        
+        alusta.add(pelimuotopaneeli);
+        alusta.add(vaikeustasopaneeli);
+        
+        aloita = new JButton("Aloita");
+        aloita.addActionListener(this);
+        alusta.add(aloita);
+        
+        alusta.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
+        alusta.pack();
+        alusta.setVisible(true);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Click!");
+        
+        if(e.getSource() == pari){
+            pelaaPariMuistipeli = true;
+        } 
+        
+        if(e.getSource() == jarjestys){
+            pelaaPariMuistipeli = false;
+        }
+        
+        if(e.getSource() == helppo){
+            pareja = 2;
+        }
+        
+        if(e.getSource() == keskitaso){
+            pareja = 4;
+        }
+        
+        if(e.getSource() == vaikea){
+            pareja = 6;
+        }
+        
+        if(e.getSource() == haaste){
+            pareja = 10;
+        }
+        
+        if(e.getSource() == aloita){
+            pelialusta = new Pelialusta(pareja, pelaaPariMuistipeli);
+            pelialusta.pelaa();
+        }
     }
 }
